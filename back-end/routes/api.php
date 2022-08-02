7df1557e9cb5a34cd31d4c88726543f957740b8c
chamsabd <chamsabdelwahed42@gmail.com>
2022-08-02 18:59:10 +0100

    add routes to middleware

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -13,7 +13,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource('/home', HomeController::class);
 });
+
+Route::post('/login', [AuthController::class, 'login']);
