3dbeff06e8936613a7dfad6d16b58732722e0a2d
chamsabd <chamsabdelwahed42@gmail.com>
2022-10-25 21:21:53 +0100

    create formation

---
+++ back-end/database/migrations/2022_08_03_205840_create_formations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formations', function (Blueprint $table) {
+            $table->id();
+            $table->string('titre');
+            $table->date('date_debut');
+            $table->string('description')->nullable();
+            $table->date('date_fin');
+
+          $table->decimal("prix");
+
+            $table->boolean('etat');
+            $table->integer('nbr_place');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formations');
+    }
+}
